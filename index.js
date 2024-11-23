78f557ffc3b2a5ed9fe7a5d3f37bd105c2043fc4
Sizhu Qu <144123630+SizhuQu@users.noreply.github.com>
2024-11-23 01:05:02 -0500

    3.6.3

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import Lab5 from "./Lab5/index.js";
 import cors from "cors";
 const app = express()
 app.use(cors());
+app.use(express.json());
 Lab5(app);
 Hello(app)
 // app.get('/hello', (req, res) => { res.send('Life is good!') })
