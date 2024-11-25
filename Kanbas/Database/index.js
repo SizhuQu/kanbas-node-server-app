608b231463165e854a815ce5ceb99536d473f917
Sizhu Qu <144123630+SizhuQu@users.noreply.github.com>
2024-11-25 18:23:03 -0500

    change

--- Kanbas/Database/index.js
+++ Kanbas/Database/index.js
@@ -3,4 +3,4 @@ import modules from "./modules.js";
 import assignments from "./assignments.js";
 import users from "./users.js";
 import enrollments from "./enrollments.js";
-export default { courses, modules, assignments, users, grades, enrollments };
+export default { courses, modules, assignments, users, enrollments };
