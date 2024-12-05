e467cee2866802018e68eff340923dfe4683753b
Sizhu Qu <144123630+SizhuQu@users.noreply.github.com>
2024-12-05 17:18:49 -0500

    3

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+import "dotenv/config";
 import express from 'express';
+import mongoose from "mongoose";
 import "dotenv/config";
 import session from "express-session";
 import Hello from "./Hello.js"
@@ -8,6 +10,10 @@ import UserRoutes from "./Kanbas/Users/routes.js";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import EnrollmentRoutes from './Kanbas/Enrollments/routes.js';
+
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/Kanbas"
+mongoose.connect(CONNECTION_STRING);
+
 const app = express()
 app.use(cors({ credentials: true,
     origin: process.env.NETLIFY_URL || "http://localhost:3000",
