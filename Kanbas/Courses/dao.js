ee6af401d9e7e6e944a1b61804a3aed8a7ba2000
Sizhu Qu <144123630+SizhuQu@users.noreply.github.com>
2024-12-10 21:56:15 -0500

    change

--- Kanbas/Courses/dao.js
+++ Kanbas/Courses/dao.js
@@ -1,9 +1,7 @@
-import Database from "../Database/index.js";
-
+import model from "./model.js";
 export function findAllCourses() {
-    return Database.courses;
+    return model.find();
 }
-
 export function findCoursesForEnrolledUser(userId) {
     const { courses, enrollments } = Database;
     return courses.filter((course) =>
@@ -11,24 +9,16 @@ export function findCoursesForEnrolledUser(userId) {
 }
 
 export function createCourse(course) {
-    const newCourse = { ...course, _id: Date.now().toString() };
-    Database.courses = [...Database.courses, newCourse];
-    return newCourse;
+    delete course._id;
+    return model.create(course);
 }
 
 export function updateCourse(courseId, courseUpdates) {
-    const { courses } = Database;
-    const course = courses.find((course) => course._id === courseId);
-    Object.assign(course, courseUpdates);
-    return course;
+    return model.updateOne({ _id: courseId }, { $set: courseUpdates });
+}
+export function deleteCourse(courseId) {
+    return model.deleteOne({ _id: courseId });
 }
 
-export function deleteCourse(courseId) {
-    const { courses, enrollments } = Database;
-    Database.courses = courses.filter((course) => course._id !== courseId);
-    Database.enrollments = enrollments.filter(
-        (enrollment) => enrollment.course !== courseId
-    );
-}
 
 
